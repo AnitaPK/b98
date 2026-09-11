047d5f353cb8bc14d3abd5d3148f290ae1ff31cd
AnitaPK <anita.khamitkar@gmail.com>
2026-09-11 11:38:29 +0530

    js - I

---
+++ javascript/progOne.js
@@ -0,0 +1,12 @@
+console.log("Hello from js file")
+// alert("Hello in Alert")
+document.write("Hello from document write")
+
+
+function digVijay(){
+document.write("Hello from document write from digVijay function")
+
+}
+
+document.getElementById("userName").textContent = "AMAR"
+
